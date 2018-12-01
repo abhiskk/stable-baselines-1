0de53eecc7ad478d919c36c82a6941bbd0e26fcf
Antonin RAFFIN <antonin.raffin@ensta.org>
2018-12-01 20:54:30 +0100

    Add SAC to tests

--- tests/test_continuous.py
+++ tests/test_continuous.py
@@ -3,7 +3,7 @@ import os
 
 import pytest
 
-from stable_baselines.a2c import A2C
+from stable_baselines import A2C, SAC
 # TODO: add support for continuous actions
 # from stable_baselines.acer import ACER
 # from stable_baselines.acktr import ACKTR
@@ -27,6 +27,7 @@ MODEL_LIST = [
     DDPG,
     PPO1,
     PPO2,
+    SAC,
     TRPO
 ]
 
@@ -43,8 +44,13 @@ def test_model_manipulation(model_class):
     try:
         env = DummyVecEnv([lambda: IdentityEnvBox(eps=0.5)])
 
+        kwargs = {}
+        if model_class == SAC:
+            # Update tradeoff between exploration/exploitation
+            kwargs = {'reward_scale': 10}
+
         # create and train
-        model = model_class(policy="MlpPolicy", env=env)
+        model = model_class(policy="MlpPolicy", env=env, **kwargs)
         model.learn(total_timesteps=NUM_TIMESTEPS, seed=0)
 
         # predict and measure the acc reward
